87d22aacef5d0afed308dc5a8931492b3a5b1ee3
Steve Grosbois <steve.grosbois@gmail.com>
2014-07-26 15:05:27 +0200

    Fix PUT problems

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 var app = angular.module('admin', [
-  'ngCookies',
-  'ngRoute',
-  'ngResource',
-  'ngSanitize'
+	'ngCookies',
+	'ngRoute',
+	'ngResource',
+	'ngSanitize'
 ]).
 config(function($httpProvider, $routeProvider) {
-  $routeProvider
-  	.when('/', {templateUrl: 'partials/index.html', controller: 'NavCtrl'})
+	$routeProvider
+		.when('/', {templateUrl: 'partials/index.html', controller: 'NavCtrl'})
 
-    .when('/login', {templateUrl: 'partials/user/login.html', controller: 'UserCtrl'})
-    .when('/users', {templateUrl: 'partials/user/all.html', controller: 'UserCtrl'})
-    .when('/users/add', {templateUrl: 'partials/user/add.html', controller: 'UserCtrl'})
-    .when('/users/:id', {templateUrl: 'partials/user/edit.html', controller: 'UserCtrl'})
+		.when('/login', {templateUrl: 'partials/user/login.html', controller: 'UserCtrl'})
+		.when('/users', {templateUrl: 'partials/user/all.html', controller: 'UserCtrl'})
+		.when('/users/add', {templateUrl: 'partials/user/add.html', controller: 'UserCtrl'})
+		.when('/users/:id', {templateUrl: 'partials/user/edit.html', controller: 'UserCtrl'})
 
-    .when('/categories', {templateUrl: 'partials/category/all.html', controller: 'CategoryCtrl'})
-    .when('/categories/add', {templateUrl: 'partials/category/add.html', controller: 'CategoryCtrl'})
-    .when('/categories/:id', {templateUrl: 'partials/category/edit.html', controller: 'CategoryCtrl'})
+		.when('/categories', {templateUrl: 'partials/category/all.html', controller: 'CategoryCtrl'})
+		.when('/categories/add', {templateUrl: 'partials/category/add.html', controller: 'CategoryCtrl'})
+		.when('/categories/:id', {templateUrl: 'partials/category/edit.html', controller: 'CategoryCtrl'})
 	.otherwise({redirectTo: '/'});
 
-  $httpProvider.defaults.useXDomain = true;
-  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
-  $httpProvider.defaults.headers.common.Authorization = '';
-  $httpProvider.defaults.transformRequest = function(data) {
-      if (data === undefined) {
-          return data;
-      }
-      return $.param(data);
-  }
+	$httpProvider.defaults.useXDomain = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	//$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+	$httpProvider.defaults.headers.common.Authorization = '';
+	/*$httpProvider.defaults.transformRequest = function(data) {
+			if (data === undefined) {
+					return data;
+			}
+			return $.param(data);
+	}*/
 });
+
+function replaceById(tab, item) {
+	for (var i = 0; i < tab.length; i++) {
+		if (tab[i].id == item.id) tab[i] = item;
+	}
+}
--- js/controllers/category.js
+++ js/controllers/category.js
@@ -1,53 +1,54 @@
 app.controller('CategoryCtrl', function ($rootScope, $scope, $location, $routeParams, CategoryFactory) {
-    $scope.inProgress = false;
+	$scope.inProgress = false;
 
-    $scope.all = function() {
-        if (!$rootScope.categories) {
-        	CategoryFactory.query(
-        		function(categories) {
-    		    	$rootScope.categories = categories;
-    			}
-    		);
-        }
-    }
+	$scope.all = function() {
+		if (!$rootScope.categories) {
+			CategoryFactory.query(
+				function(categories) {
+					$rootScope.categories = categories;
+				}
+			);
+		}
+	}
 
-    $scope.edit = function() {
-    	CategoryFactory.get({id : $routeParams.id},
-    		function (category) {
-                delete category.$promise;
-                delete category.$resolved;
-		    	$scope.category = category;
+	$scope.edit = function() {
+		CategoryFactory.get({id : $routeParams.id},
+			function (category) {
+				delete category.$promise;
+				delete category.$resolved;
+				$scope.category = category;
 			}
 		);
-    }
+	}
 
-    $scope.save = function() {
-    	CategoryFactory.update(
-    		{id : $scope.category.id}, 
-    		{name: $scope.category.name},
-    		function(category) {
-		    	$location.path('/categories');
+	$scope.save = function() {
+		CategoryFactory.update(
+			{id : $scope.category.id}, 
+			{name: $scope.category.name},
+			function(category) {
+				replaceById($rootScope.categories, category);
+				$location.path('/categories');
 			}
 		);
-    }
+	}
 
-    $scope.create = function() {
-    	CategoryFactory.save(null, 
-    		$scope.category,
-    		function(category) {
-                $rootScope.categories.push(category);
-		    	$location.path('/categories');
+	$scope.create = function() {
+		CategoryFactory.save(null, 
+			$scope.category,
+			function(category) {
+				$rootScope.categories.push(category);
+				$location.path('/categories');
 			}
 		);
-    }
+	}
 
-    $scope.delete = function(category) {
-    	CategoryFactory.remove(
-    		{id : category.id}, 
-    		function() {
-		    	var index = $rootScope.categories.indexOf(category);
-  				$rootScope.categories.splice(index, 1);
+	$scope.delete = function(category) {
+		CategoryFactory.remove(
+			{id : category.id}, 
+			function() {
+				var index = $rootScope.categories.indexOf(category);
+				$rootScope.categories.splice(index, 1);
 			}
 		);
-    }
+	}
 });
